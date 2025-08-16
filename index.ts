417376dcde209899b065b1bdc39377ea45103134
Khue Nguyen <khuezy.nguyen@gmail.com>
2025-08-16 16:32:04 -0700

    Update repo with transaction bug

--- index.ts
+++ index.ts
@@ -1,11 +1,10 @@
 import { type LibSQLDatabase, drizzle } from 'drizzle-orm/libsql'
 import { type Client, type Config, createClient } from '@libsql/client'
 import { sql } from 'drizzle-orm'
-
-
+import assert from 'node:assert'
 const DB_PATH = '/tmp/data/bug.db'
-const SYNC_URL = 'libsql://<db>.aws-us-east-1.turso.io'
-const AUTH_TOKEN = ''
+const SYNC_URL = 'libsql://dev-khuezy.aws-us-east-1.turso.io'
+const AUTH_TOKEN = 'eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NTQwNzExNDUsImlkIjoiMWExZmE5ODEtNGNjZS00MDZhLWFhYTYtODVhZTI0NGJlOWEzIiwicmlkIjoiZjAzMDE5MjUtOWQ2MS00ODdmLWJkZDktYjk5MjBhZDYwZDI3In0.rNCxmXPlfw8Jh2MkAgvNFbSrIytUatE67ekaVIsuQew_YnLVWjQY_dRWYnoMkX53RgACEM0R3vY5VZjxeyuyAg'
 
 
 async function getNormalClient() {
@@ -27,7 +26,7 @@ async function getEmbeddedReplicaClient() {
     authToken: AUTH_TOKEN,
     syncInterval: 60
   })
-  await erClient.sync()
+  // await erClient.sync()
   const erDB = drizzle({
     client: erClient
   })
@@ -49,18 +48,27 @@ async function main() {
   // Embedded Replica client is having issues...
   const embeddedClient = await getEmbeddedReplicaClient()
 
-  // First we insert a base record, subsequent inserts will be no op
-  const insert = await embeddedClient.run(sql`INSERT into _test(id, number) values(1, 1) ON CONFLICT(id) DO NOTHING RETURNING *;`)
+  // First we insert a base record
+  await embeddedClient.run(sql`INSERT into _test(id, number) values(1, 1) ON CONFLICT(id) DO UPDATE SET number = 1;`)
 
-  // Then update "number" by one each time
-  const update = await embeddedClient.run(sql`UPDATE _test set number = number + 1  RETURNING *`)
-  console.log('UPDATE returns updated value: ', update.rows[0])
+  // Update the row, set `number` to 2
+  await embeddedClient.run(sql`UPDATE _test set number = 2;`)
 
-  // Finally query the table. With this query, turso client update the local `/tmp/data/bug.db`
-  // ATTN: comment the next two lines out and rerun the script... 
-  //       Without this query, the local client never gets updated.
-  const query = await embeddedClient.run(sql`SELECT * FROM _test;`)
-  console.log('SELECT result: ', query.rows[0])
+  // Query row, it should get the updated value
+  // Turso should sync the values on read (lazy)
+  const select = await embeddedClient.run(sql`SELECT * from _test;`)
+  console.log('SELECT returns latest value: ', select.rows[0])
+  assert.equal(select.rows[0].number, 2, 'Value should be 2')
+
+  // Run the update inside a transaction
+  await embeddedClient.transaction(async tx => {
+    return tx.run('UPDATE _test set number = 9;')
+  })
+
+  // Query the row, it should get the updated value but it doesn't
+  const selectAfterTxn = await embeddedClient.run(sql`SELECT * from _test;`)
+  console.log('SELECT returns latest value: ', selectAfterTxn.rows[0])
+  assert.equal(select.rows[0].number, 9, `Value should be 9 but got ${select.rows[0].number}`)
 }
 
 main()
